e708cd9a2fffa812382d44928332ab17f610ad78
Vantuir Paulo <vantuirpaulo@outlook.com>
2021-09-28 15:29:33 -0300

    chore: add bar code converter

--- __test__/unit/boletoParser.spec.ts
+++ __test__/unit/boletoParser.spec.ts
@@ -2,7 +2,9 @@ import * as boletoParser from '../../src/utils/boletoParser';
 
 const data = {
   validBoletoTitulo: '21290001192110001210904475617405975870000002000',
+  validTituloBarCode: '21299758700000020000001121100012100447561740',
   validBoletoConvenio: '836300000012718701380013223258756111080992882466',
+  validConvenioBarCode: '83630000001718701380012232587561108099288246',
 };
 
 describe('Boleto de Titulo parser', () => {
@@ -29,5 +31,30 @@ describe('Boleto de Convênio parser', () => {
       '223258756111',
       '080992882466',
     ]);
+  });
+});
+
+describe('Convert a typed line into a bar code', () => {
+  it('should be convert a typed line of boleto de convênio into a bar code', () => {
+    const result = boletoParser.tituloToBarCode([
+      '2129000119',
+      '21100012109',
+      '04475617405',
+      '9',
+      '75870000002000',
+    ]);
+
+    expect(result).toEqual(data.validTituloBarCode);
+  });
+
+  it('shoud be convert a typed line of boleto de convênio into a bar code', () => {
+    const result = boletoParser.convenioToBarCode([
+      '836300000012',
+      '718701380013',
+      '223258756111',
+      '080992882466',
+    ]);
+
+    expect(result).toEqual(data.validConvenioBarCode);
   });
 });
--- src/utils/boletoParser.ts
+++ src/utils/boletoParser.ts
@@ -1,6 +1,6 @@
 export const parseTitulo = (code: string): Array<string> => {
   const fields = [];
-  const lastDigit = code.length +1;
+  const lastDigit = code.length + 1;
 
   fields.push(code.slice(0, 10));
   fields.push(code.slice(10, 21));
@@ -13,7 +13,7 @@ export const parseTitulo = (code: string): Array<string> => {
 
 export const parseConvenio = (code: string): Array<string> => {
   const fields = [];
-  const lastDigit = code.length +1;
+  const lastDigit = code.length + 1;
 
   fields.push(code.slice(0, 12));
   fields.push(code.slice(12, 24));
@@ -21,4 +21,27 @@ export const parseConvenio = (code: string): Array<string> => {
   fields.push(code.slice(36, lastDigit));
 
   return fields;
+};
+
+export const tituloToBarCode = (fields: Array<string>): string => {
+
+  const fieldA = fields[0].slice(0, 3);
+  const fieldB = fields[0].slice(3, 4);
+  const fieldC = fields[0].slice(4, 9);
+  const fieldX = fields[0].slice(9, 10);
+  const fieldD = fields[1].slice(0, 10);
+  const fieldE = fields[2].slice(0, 10);
+  const fieldU = fields[4].slice(0, 4);
+  const fieldV = fields[4].slice(4, 14);
+
+  return fieldA + fieldB + fieldX + fieldU + fieldV + fieldC + fieldD + fieldE;
+};
+
+export const convenioToBarCode = (fields: Array<string>): string => {
+  const fieldA = fields[0].slice(0, -1);
+  const fieldB = fields[1].slice(0, -1);
+  const fieldC = fields[2].slice(0, -1);
+  const fieldD = fields[3].slice(0, -1);
+
+  return fieldA + fieldB + fieldC + fieldD;
 }
